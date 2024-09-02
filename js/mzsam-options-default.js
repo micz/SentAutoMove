30605f605cb3a00c7d94e57d2f7b363cde093ea0
Mic <m@micz.it>
2024-09-02 22:33:11 +0200

    todo added to default options

--- js/mzsam-options-default.js
+++ js/mzsam-options-default.js
@@ -1,9 +1,10 @@
 export const prefs_default = {
     'do_debug': false,
     'do_only_same_account': true,
-    'do_only_sent': true,
+    'do_only_sent': true,   // TODO
     'dest_folder_type': 'subfolder',  // allowed values: 'subfolder', 'same_folder'
     'dest_folder_prefix': '',
     'ignore_archive_folders': true,
     'min_moves_to_report': 3,
+    'max_messages_moved': 0,  //TODO
 }
