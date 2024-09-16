4d96c6e0f8648d7bc764509376d0186b601417ce
Mic <m@micz.it>
2024-09-16 21:07:28 +0200

    new options added

--- js/mzsam-options-default.js
+++ js/mzsam-options-default.js
@@ -3,11 +3,15 @@ export const prefs_default = {
     'do_only_same_account': true,
     'do_only_sent': true,
     'do_only_sent_folders': true,
+    'use_also_thread_index': false,
     'dest_folder_type': 'subfolder',  // allowed values: 'subfolder', 'same_folder'
     'dest_folder_prefix': '',
+    'dest_folder_ok_same_folder_with_prefix': true,
     'ignore_archive_folders': true,
     'min_moves_to_open_report_tab': 10,
     'max_messages_moved': 20,
+    'pause_between_messages': 200,
+    'pause_every_10_messages': 1000,
     'warn_before_run': true,
     '_internal__ask_empty_prefix_done': false,
 }
